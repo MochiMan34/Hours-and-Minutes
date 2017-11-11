911dd6b3bf96d935013364ef4dcea1e549ada4a8
MochiMan34 <st27567@email.vccs.edu>
2017-11-10 22:55:05 -0500

    Add files via upload

---
+++ HoursAndMinutes/HoursAndMinutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursAndMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculateHoursAndMinutes(); 
+        }
+
+        public static void calculateHoursAndMinutes()
+        {
+            int minutes = 198;
+            int hours = minutes / 60;
+            int leftOverMinutes = minutes % 60;
+
+            System.Console.WriteLine(minutes  + " Minutes Becomes: " + hours + " Hours and " + leftOverMinutes + " minutes" );
+            System.Console.ReadKey(); 
+        }
+    }
+}
